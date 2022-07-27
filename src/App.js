713d3487cb6c786a482492ab4c445dda5eed5b5a
ad551 <gdaim551@gmail.com>
2022-07-27 17:20:04 +0500

    Code Cleaning

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { Button } from 'antd';
 import 'antd/dist/antd.css';
 import './App.css';
 
-import ShowCase from './ShowCase';
-
 function App() {
   const [count, setCount] = useState(0);
   const [errorMsg, setErrorMsg] = useState("Welcome to Counter App!")
@@ -13,26 +11,27 @@ function App() {
     <>
     <div className='app-main-div'>
       <h1 className='count-h1'>{count}</h1>
-      <Button type="primary" className='inc-Button' 
-      onClick={()=>{
-        setCount(count+1)
-        setErrorMsg("Increment Successful!")
-      }}>Increment</Button>
-      
-      <Button type="primary" className='dec-Button' 
-      onClick={()=>{
-        if(count>0){
-          setCount(count-1)
-          setErrorMsg("Decrement Successful!")
-        }
-        else{
-          setErrorMsg("Cannot decrement when zero!")
-        }
-      }}>Decrement</Button>
+      <div className="display-div">
+        <Button type="primary" className='inc-Button' 
+        onClick={()=>{
+          setCount(count+1)
+          setErrorMsg("Increment Successful!")
+        }}>Increment</Button>
+        
+        <Button type="primary" className='dec-Button' 
+        onClick={()=>{
+          if(count>0){
+            setCount(count-1)
+            setErrorMsg("Decrement Successful!")
+          }
+          else{
+            setErrorMsg("Cannot decrement when zero!")
+          }
+        }}>Decrement</Button>
+      </div>
 
       <p className='error-p'>{errorMsg}</p>
     </div>
-    <ShowCase/>
     </>
   );
 }
